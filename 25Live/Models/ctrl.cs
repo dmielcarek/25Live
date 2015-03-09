02958c7eea37379027eaac183c1c05455b920d01
Tripti Sharma <tripti.sharma@bellevuecollege.edu>
2015-03-09 09:54:43 -0700

    Allow datain file create job run manually by user.

--- 25Live/Models/ctrl.cs
+++ 25Live/Models/ctrl.cs
@@ -6,11 +6,16 @@ using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using System.Collections;
 
 namespace _25Live.Models
 {
     public class ctrl
     {
+        //Read all the config variables from web.config file
+        
+
 
         public dynamic ReadAllSettings()
         {
@@ -64,13 +69,9 @@ namespace _25Live.Models
             {
                 DataTable dt = new DataTable();
                 String cs = System.Configuration.ConfigurationManager.ConnectionStrings["ClassDal"].ConnectionString;
-                //SqlDataReader reader;
                 SqlConnection con = new SqlConnection(cs);
                 SqlCommand com = new SqlCommand("Execute usp_25LiveDataInFile", con);
-                //cmd.CommandText = "usp_25LiveDataInFile";
-                //cmd.CommandType = CommandType.StoredProcedure;
                 com.Connection = con;
-                //return (Boolean)com.ExecuteScalar();
                 con.Open();
 
                 SqlDataAdapter da = new SqlDataAdapter(com);
@@ -131,6 +132,183 @@ namespace _25Live.Models
         public String GetTimestamp(DateTime value)
         {
             return value.ToString("yyyyMMddHHmmss");
+        }
+
+        public IDictionary<string, string> createDataInFile()
+        {
+            IDictionary<string, string> dict = new Dictionary<string, string>(); // Will be used for return values
+            String message = "";
+            
+            try
+            {
+
+                var allConfigurations = ReadAllSettings();
+                var colDepartmentID = int.Parse(allConfigurations["ColDepartmentId"]);
+                var lenDepartmentID = int.Parse(allConfigurations["LengthDepartmentId"]);
+                var colCatalog = int.Parse(allConfigurations["ColCatalog"]);
+                var lenCatalog = int.Parse(allConfigurations["LengthCatalog"]);
+                var colSection = int.Parse(allConfigurations["ColSection"]);
+                var lenSection = int.Parse(allConfigurations["LengthSection"]);
+                var colDays = int.Parse(allConfigurations["ColDays"]);
+                var lenDays = int.Parse(allConfigurations["LengthDays"]);
+                var colStartHours = int.Parse(allConfigurations["ColStartHours"]);
+                var colStartMinutes = int.Parse(allConfigurations["ColStartMinutes"]);
+                var colFinishtHours = int.Parse(allConfigurations["ColFinishtHours"]);
+                var colFinishtMinutes = int.Parse(allConfigurations["ColFinishtMinutes"]);
+                var colAP = int.Parse(allConfigurations["ColAP"]);
+                var colEnrollment = int.Parse(allConfigurations["ColEnrollment"]);
+                var lenEnrollment = int.Parse(allConfigurations["LengthEnrollment"]);
+                var colRoomName = int.Parse(allConfigurations["ColRoomName"]);
+                var lenRoomName = int.Parse(allConfigurations["LengthRoomName"]);
+                var colBeginYear = int.Parse(allConfigurations["ColBeginYear"]);
+                var colBeginMonth = int.Parse(allConfigurations["ColBeginMonth"]);
+                var colBeginDay = int.Parse(allConfigurations["ColBeginDay"]);
+                var colEndYear = int.Parse(allConfigurations["ColEndYear"]);
+                var colEndMonth = int.Parse(allConfigurations["ColEndMonth"]);
+                var colEndDay = int.Parse(allConfigurations["ColEndDay"]);
+                var colMeeting = int.Parse(allConfigurations["ColMeeting"]);
+                var lenMeeting = int.Parse(allConfigurations["LengthMeeting"]);
+                var colASM = int.Parse(allConfigurations["ColAssignment"]);
+                var lenASM = int.Parse(allConfigurations["LengthAssignment"]);
+                var colEvent = int.Parse(allConfigurations["ColEvent"]); // This can be ignored and leave space)s
+                var lenEvent = int.Parse(allConfigurations["LengthEvent"]);
+                var colInstructor = int.Parse(allConfigurations["ColInstructor"]);
+                var lenInstructor = int.Parse(allConfigurations["LengthInstructor"]);
+                var colCourse = int.Parse(allConfigurations["ColCourse"]);
+                var lenCourse = int.Parse(allConfigurations["LengthCourse"]);
+                var colTerm = int.Parse(allConfigurations["ColTerm"]);
+                var lengthTerm = int.Parse(allConfigurations["LengthTerm"]);
+                var colCRN = int.Parse(allConfigurations["ColCRN"]);
+                var lenCRN = int.Parse(allConfigurations["LengthCRN"]);
+
+                //Call the stored procedure to get data
+                //loop through the records
+                String classRows = getData();
+                
+
+                if (!string.IsNullOrEmpty(classRows))
+                {
+                    var objects = JArray.Parse(classRows); // parse as array 
+                    var codeRanFlag = 0;
+                    foreach (JObject root in objects)
+                    {
+                        var roomName = root.GetValue("RoomName").ToString().Trim();
+                        var days = root.GetValue("Days").ToString().Trim();
+                        var classID = root.GetValue("ClassID").ToString().Trim();
+                        var startHours = root.GetValue("StartHours").ToString().Trim();
+                        var startMinutes = root.GetValue("StartMinutes").ToString().Trim();
+                        var finishHours = root.GetValue("FinishHours").ToString().Trim();
+                        var finishMinutes = root.GetValue("FinishMinutes").ToString().Trim();
+                        var apDesignator = root.GetValue("APDesignator").ToString().Trim();
+                        var enrollment = root.GetValue("Enrollment").ToString().Trim();
+                        var departmentID = root.GetValue("DepartmentID").ToString().Trim();
+                        var startWeek = root.GetValue("StartWeek").ToString().Trim();
+                        var finishWeek = root.GetValue("FinishWeek").ToString().Trim();
+                        var crn = root.GetValue("CRN").ToString().Trim();
+                        var courseName = root.GetValue("CourseName").ToString().Trim();
+                        var section = root.GetValue("Section").ToString().Trim();
+                        var catalog = root.GetValue("Catalog").ToString().Trim(); // Course Number
+                        var term = root.GetValue("term").ToString().Trim(); // YearQuarterID
+                        var instructorName = root.GetValue("InstructorName").ToString().Trim();
+                        var assignment = root.GetValue("AssignmentField").ToString().Trim();
+                        var meetingNumber = root.GetValue("MeetingNumber").ToString().Trim();
+                        //generate line
+                        string line = "";
+                        var spacesToAdd = 0; // Initialising it for reuse.
+                        //Adding the spaces to make the field with length as specifed in lenField
+                        spacesToAdd = lenDepartmentID - departmentID.Length;
+                        if (spacesToAdd > 0)
+                            departmentID = departmentID.PadRight(departmentID.Length + spacesToAdd, ' ');
+
+                        line += departmentID;
+                        line = concatNewField(line, colCatalog, lenCatalog, catalog);
+                        line = concatNewField(line, colSection, lenSection, section);
+                        line = concatNewField(line, colDays, lenDays, days);
+                        line = concatNewField(line, colStartHours, startHours.Length, startHours);
+                        line = concatNewField(line, colStartMinutes, startMinutes.Length, startMinutes);
+                        line = concatNewField(line, colFinishtHours, finishHours.Length, finishHours);
+                        line = concatNewField(line, colFinishtMinutes, finishMinutes.Length, finishMinutes);
+                        line = concatNewField(line, colAP, apDesignator.Length, apDesignator);
+                        line = concatNewField(line, colEnrollment, lenEnrollment, enrollment);
+                        line = concatNewField(line, colRoomName, lenRoomName, roomName);
+                        line = concatNewField(line, colBeginYear, startWeek.Length, startWeek);
+                        line = concatNewField(line, colEndYear, finishWeek.Length, finishWeek);
+                        line = concatNewField(line, colMeeting, lenMeeting, meetingNumber);
+                        line = concatNewField(line, colASM, lenASM, assignment);
+                        var Event = ""; // This field is optional and we will just fill this with spaces.
+                        line = concatNewField(line, colEvent, lenEvent, Event);
+                        line = concatNewField(line, colInstructor, lenInstructor, instructorName);
+                        line = concatNewField(line, colCourse, lenCourse, courseName);
+                        line = concatNewField(line, colTerm, lengthTerm, term);
+                        line = concatNewField(line, colCRN, lenCRN, crn);
+                        line += "\n";//System.Environment.NewLine;
+
+
+
+                        //Write to a file
+                        term = term.ToString().Trim();
+                        var filename = term + ".dat";
+                        string path = allConfigurations["FilePath"] + filename;
+
+                        if (codeRanFlag == 0)
+                        {
+                            // Check if the file exists.
+                            //If it exists that archive that file and create a new file.
+                            var archivePath = allConfigurations["ArchivePath"];
+                            if (System.IO.File.Exists(path))
+                            {
+                                String timeStamp = GetTimestamp(DateTime.Now);
+                                var newFileName = term + "_" + timeStamp + ".dat";
+                                var newPath = archivePath + newFileName;
+                                // Move the existing file to archive folder
+                                System.IO.File.Move(path, newPath);
+                                message += "File " + newFileName + " has been moved to " + archivePath + ".";
+
+                            }
+                            //Create new file and write the line in it
+                            if (!System.IO.File.Exists(path))
+                            {
+                                System.IO.StreamWriter file = new System.IO.StreamWriter(path);
+                                message += " A new file named " + filename + " is created at location " + allConfigurations["FilePath"] + ".";
+                                //TextWriter tw = new StreamWriter(path);
+                                file.WriteLine(line);
+                                file.Close();
+                            }
+                            codeRanFlag = 1;
+                        }
+                        else
+                        {
+                            // Here ideally you should just be appending data but I am still checking if the file is not created to be safe
+
+                            if (!System.IO.File.Exists(path))
+                            {
+                                System.IO.StreamWriter file = new System.IO.StreamWriter(path);
+                                file.WriteLine(line);
+                                file.Close();
+                            }
+                            else if (System.IO.File.Exists(path))
+                            {
+                                System.IO.StreamWriter file = new System.IO.StreamWriter(path, true);
+                                file.WriteLine(line);
+                                file.Close();
+                            }                          
+                           
+                        }
+                    }
+                }
+                //Session["message"] = message;
+                dict["message"] = message;
+                dict["status"] = "success";
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception", ex);
+                //Session["exception"] = ex.Message.ToString();
+                dict["message"] = ex.Message.ToString();
+                dict["status"] = "failure";
+
+            }
+            return dict;
         }
     }
 }
